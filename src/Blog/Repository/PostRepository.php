8a48fc6d179f0bccebfdca61fb130ed45d2b641a
Lucas de Oliveira <94oliveira.lucas@gmail.com>
2016-09-20 08:24:17 -0300

    Update PostRepository.php

--- src/Blog/Repository/PostRepository.php
+++ src/Blog/Repository/PostRepository.php
@@ -34,7 +34,7 @@ class PostRepository
     /**
      * @param Post $post
      */
-    public function save(Post $post) : void
+    public function save(Post $post)
     {
         $this->dm->persist($post);
         $this->dm->flush();
